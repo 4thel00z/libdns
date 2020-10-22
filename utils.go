41deb73c597c931634a350be2cb368e26b4829c4
4thel00z <4thel00z@gmail.com>
2020-10-22 03:30:33 +0200

    [master] Add generic utils library

---
+++ utils.go
@@ -0,0 +1,15 @@
+package libdns
+
+import (
+	"fmt"
+)
+
+func PrintlnUint16WithZeroPad(value uint16) {
+	fmt.Printf("%016b\n", value)
+}
+func PrintlnUint32WithZeroPad(value uint32) {
+	fmt.Printf("%032b\n", value)
+}
+func PrintlnUint64WithZeroPad(value uint64) {
+	fmt.Printf("%064b\n", value)
+}
