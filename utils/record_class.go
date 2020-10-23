30f66cd682b3613466a8b576de07b1e836bae49d
4thel00z <4thel00z@gmail.com>
2020-10-24 00:25:06 +0200

    [master] Add query record class constants

---
+++ utils/record_class.go
@@ -0,0 +1,11 @@
+package utils
+
+const (
+	InternetClass = 0x0001
+	CSNetClass    = 0x0002
+	ChaosClass    = 0x0003
+	HESIODClass   = 0x0004
+	NoneClass     = 0x00fe
+	AllClass      = 0x00ff
+	AnyClass      = 0x00ff
+)
