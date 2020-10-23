e26e8c4cb48ada34a8735a2576ce22efe763be53
4thel00z <4thel00z@gmail.com>
2020-10-23 02:42:53 +0200

    [master] Adjust tests

--- dns/models_test.go
+++ dns/models_test.go
@@ -18,12 +18,38 @@ func TestNoInit(t *testing.T) {
 	}
 }
 
+func TestQueryEncode(t *testing.T) {
+
+	query := Query{
+		ID:           0b1111111111111111,
+		QR:           true,
+		Opcode:       0,
+		AA:           false,
+		TC:           false,
+		RD:           false,
+		RA:           false,
+		Z:            false,
+		AD:           true,
+		CD:           true,
+		ResponseCode: 0b1111,
+		QDCount:      63,
+		ANCount:      1337,
+		NSCount:      0,
+		ARCount:      0b1111111111111111,
+		Questions:    nil,
+	}
+	_, err := query.Encode()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestDecodeQuery(t *testing.T) {
 	rawQuery := []byte{
 		0b11111111, 0b11111111,
 		//1:QR|4:Opcode|1:AA|1:TC|1:RD|1:RA|1:Z|1:AD|1:CD|4:RCODE
 		0b00000000, 0b00111111,
-		0b00000000, 0b00111111,
+		0b00000000, 0b00000000,
 		0b00000101, 0b00111001,
 		0b00000000, 0b00000000,
 		0b11111111, 0b11111111,
@@ -45,7 +71,7 @@ func TestDecodeQuery(t *testing.T) {
 		AD:           true,
 		CD:           true,
 		ResponseCode: 0b1111,
-		QDCount:      63,
+		QDCount:      0,
 		ANCount:      1337,
 		NSCount:      0,
 		ARCount:      0b1111111111111111,
@@ -53,17 +79,15 @@ func TestDecodeQuery(t *testing.T) {
 	}
 
 	assert.Equal(t, expected, dnsQuery)
-	/*
-		TODO: this needs to work, but Encode is broken..
 
-		encoded, err := dnsQuery.Encode()
-		if err != nil {
-			t.Fatal(err)
-		}
-		query, err := DecodeQuery(encoded)
-		if err != nil {
-			t.Fatal(err)
-		}
-		assert.Equal(t, expected, query)*/
+	encoded, err := dnsQuery.Encode()
+	if err != nil {
+		t.Fatal(err)
+	}
+	query, err := DecodeQuery(encoded)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, expected, query)
 
 }
