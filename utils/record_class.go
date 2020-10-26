08305dac71f8d22c0a823a826abf06aecbc86d15
4thel00z <4thel00z@gmail.com>
2020-10-27 00:20:09 +0100

    [master] Add typing to the record_class module enum

--- utils/record_class.go
+++ utils/record_class.go
@@ -1,11 +1,13 @@
 package utils
 
+type RecordClass int
+
 const (
-	InternetClass = 0x0001
-	CSNetClass    = 0x0002
-	ChaosClass    = 0x0003
-	HESIODClass   = 0x0004
-	NoneClass     = 0x00fe
-	AllClass      = 0x00ff
-	AnyClass      = 0x00ff
+	InternetClass = RecordClass(0x0001)
+	CSNetClass    = RecordClass(0x0002)
+	ChaosClass    = RecordClass(0x0003)
+	HESIODClass   = RecordClass(0x0004)
+	NoneClass     = RecordClass(0x00fe)
+	AllClass      = RecordClass(0x00ff)
+	AnyClass      = RecordClass(0x00ff)
 )
