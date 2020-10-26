ec4b39ffe742a48f56a2fed23cb15a6c5207d991
4thel00z <4thel00z@gmail.com>
2020-10-27 00:22:13 +0100

    [master] Add first high-level API SimpleQueryOnce

--- api.go
+++ api.go
@@ -1,2 +1,43 @@
 package libdns
 
+import (
+	"bufio"
+	"github.com/4thel00z/libdns/dns"
+	"github.com/4thel00z/libdns/utils"
+)
+
+func SimpleQueryOnce(d utils.DNSServer, domain string, t utils.RecordType, c utils.RecordClass, timeout int64) (dns.Response, error) {
+	err := dns.Init()
+	if err != nil {
+		return dns.Response{}, err
+	}
+	conn, err := d.ToConnection(timeout)
+	if err != nil {
+		return dns.Response{}, err
+	}
+
+	q := dns.Question{
+		Domain: domain,
+		Type:   uint16(t),
+		Class:  uint16(c),
+	}
+
+	query := dns.Query{
+		ID:        0xAAAA,
+		RD:        true,
+		Questions: []dns.Question{q},
+	}
+
+	n, err := query.Write(conn)
+	if err != nil {
+		return dns.Response{}, err
+	}
+
+	encodedAnswer := make([]byte, n)
+	if _, err := bufio.NewReader(conn).Read(encodedAnswer); err != nil {
+		return dns.Response{}, err
+	}
+
+	decodeQuery, err := dns.DecodeQuery(encodedAnswer)
+	return dns.Response(decodeQuery), err
+}
