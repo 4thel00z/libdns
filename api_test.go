67809062ad237a6665f955382b5e88019ff42095
4thel00z <4thel00z@gmail.com>
2020-10-27 00:22:27 +0100

    [master] Add tests for SimpleQueryOnce

--- api_test.go
+++ api_test.go
@@ -1,1 +1,16 @@
 package libdns
+
+import (
+	"fmt"
+	"github.com/4thel00z/libdns/utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSimpleQueryOnce(t *testing.T) {
+	response, err := SimpleQueryOnce(utils.CloudflarePrimary, "google.de", utils.A, utils.InternetClass, 10)
+	assert.Nil(t, err)
+	assert.Greater(t, response.ANCount, uint16(0))
+	assert.Greater(t, response.QDCount, uint16(0))
+	fmt.Printf("%v", response)
+}
